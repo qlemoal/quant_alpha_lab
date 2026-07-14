e4f98343ca0dc9ced35ddc109f40c3b0c4cb0bad
Quentin Le Moal <qs.lemoal@gmail.com>
2026-07-14 18:56:09 +0200

    fixed bugs in inspect_features.ipynb

--- src/features/beta.py
+++ src/features/beta.py
@@ -1,2 +1,13 @@
 from polars import col as c
 
+##  NOT READY
+
+def add_mkt_logret(lf):
+    market_logret = (
+        lf.group_by('date')
+        .agg(c('logret').mean())
+        .alias('mkt_logret')
+    )
+    lf = lf.join(market_logret)
+
+def add_beta(lf):
